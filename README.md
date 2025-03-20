01605c912b734645d22ac776b9907bc2385a5368
Raphael Nask <raphael_nask@hotmail.com>
2025-03-20 00:47:41 -0300

    Implementa software com monitoramento de rede.

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# network-sniffer
+# Network-Sniffer
+
+Este script permite monitorar o tráfego de rede de uma máquina, exibindo informações sobre pacotes recebidos e enviados, dispositivos conectados à rede, e outros dados de rede relevantes. Ele pode ser usado para monitorar o tráfego de rede entre hosts específicos ou em toda a rede local.
+
+## Funcionalidades:
+
+- **Monitoramento de Pacotes**: Exibe o número de pacotes recebidos e enviados.
+- **Dispositivos Conectados**: Exibe uma tabela com os dispositivos conectados à rede, incluindo o endereço IP e o endereço MAC.
+- **Gráficos de Tráfego**: Exibe gráficos em tempo real com a quantidade de pacotes recebidos e enviados.
+- **Últimos Pacotes**: Exibe informações sobre os últimos 10 pacotes capturados.
+- **Controle de Usuário**: Permite sair do script pressionando a tecla "Tab".
+
+## Requisitos:
+
+- **Python 3.x**
+- **Bibliotecas**:
+  - `scapy`
+  - `rich`
+  - `asciichartpy`
+  - `keyboard`
+  
+## Instalação das dependências:
+
+O script requer a instalação de algumas bibliotecas. Elas podem ser instaladas utilizando um gerenciador de pacotes do Python, como o `pip`. 
+
+```bash
+pip install -r .\requirements.txt
+```
+
+### Instalação do Npcap
+
+Se você estiver utilizando o Windows, o **Npcap** é necessário para a captura de pacotes. O Npcap fornece a interface de captura de pacotes de rede para o Scapy. 
+
+1. Baixe o Npcap [aqui](https://npcap.com/#download).
+2. Durante a instalação, certifique-se de selecionar a opção **"WinPcap API-compatible Mode"** para garantir que o Scapy consiga capturar pacotes.
+
+
+## Como usar:
+
+1. **Monitore toda a rede**:
+   - O script monitora toda a rede local por padrão.
+
+2. **Monitore tráfego entre dois hosts específicos**:
+   - O script também pode ser configurado para monitorar o tráfego entre dois hosts específicos. Para isso, basta adicionar os endereços IP dos hosts diretamente no código.
+
+3. **Controle de saída**:
+   - Durante a execução do script, pressione a tecla `Tab` para sair do programa.
+
+## Como funciona:
+
+- O script captura pacotes de rede utilizando a biblioteca `scapy`.
+- Utiliza ARP para descobrir dispositivos na rede e exibe uma tabela com os dispositivos conectados.
+- Usa gráficos em tempo real para exibir o número de pacotes recebidos e enviados.
+- Exibe as últimas 10 informações de pacotes capturados.
+
+## Exemplo de Exibição:
+
+- **Gráficos de Pacotes**: Gráficos em tempo real mostrando a quantidade de pacotes recebidos e enviados.
+- **Tabela de Dispositivos**: Lista de dispositivos conectados à rede com IP e MAC.
+- **Últimos Pacotes**: Exibe informações sobre os últimos 10 pacotes capturados, com origem e destino.
+
+## Contribuindo:
+
+Se você deseja contribuir para este projeto, basta realizar um fork deste repositório, fazer as alterações desejadas e abrir um pull request. Fique à vontade para adicionar novos recursos ou melhorar a documentação.
+
+## Créditos
+
+Esse software foi inspirado no repositório [Pynetmon](https://github.com/Sr-vZ/pynetmon/tree/main), apenas melhorei alguns aspectos do software e adicionei mais funcionalidades.
---
+++ src/network-sniffer.py
@@ -0,0 +1,185 @@
+import time
+import os
+import threading
+import scapy.all as scapy
+import asciichartpy as acp
+from rich import print
+from rich import box
+from rich.live import Live
+from rich.panel import Panel
+from rich.layout import Layout
+from rich.align import Align
+from rich.table import Table
+import keyboard as kb
+import collections
+
+default_config = {"graph_width": 100, "panel_border": "#d600ff"}  # Aumentando a largura dos gráficos
+exit_script = False
+packet_counts = {"incoming": 0, "outgoing": 0}
+last_10_packets = collections.deque(maxlen=10)  # Para armazenar os últimos 10 pacotes
+
+# Configuração para armazenar pacotes recebidos e enviados com limite
+MAX_PACKETS = 20
+packet_buffer_in = collections.deque(maxlen=MAX_PACKETS)
+packet_buffer_out = collections.deque(maxlen=MAX_PACKETS)
+
+# Endereços IP dos hosts que serão monitorados (se fornecidos)
+MONITORED_HOSTS = []
+
+# Captura pacotes e conta tráfego de entrada e saída
+def packet_sniffer(packet):
+    global packet_counts, last_10_packets
+
+    # Filtra pacotes entre hosts específicos ou entre todos os pacotes da rede
+    if packet.haslayer(scapy.IP):
+        src_ip = packet[scapy.IP].src
+        dst_ip = packet[scapy.IP].dst
+
+        # Se MONITORED_HOSTS estiver vazio, captura pacotes da rede inteira
+        if not MONITORED_HOSTS or (src_ip in MONITORED_HOSTS and dst_ip in MONITORED_HOSTS):
+            packet_info = {"src": src_ip, "dst": dst_ip}
+            last_10_packets.append(packet_info)
+
+            if dst_ip == scapy.get_if_addr("Ethernet"): # Interface de rede monitorada
+                packet_counts["incoming"] += 1
+            else:
+                packet_counts["outgoing"] += 1
+
+# Função para iniciar a captura de pacotes em uma thread separada
+def start_sniffing():
+    scapy.sniff(prn=packet_sniffer, store=False, iface="Ethernet") # Interface de rede monitorada
+
+# Descobre dispositivos na rede usando ARP Scan
+def discover_devices():
+    devices = []
+    arp_request = scapy.ARP(pdst="192.168.0.1/24")  # Utilize o Gateway da rede
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    for element in answered_list:
+        devices.append({"IP": element[1].psrc, "MAC": element[1].hwsrc})
+    
+    return devices
+
+# Monta tabela de dispositivos conectados
+def create_device_table():
+    table = Table(title="Dispositivos na Rede", box=box.ROUNDED, border_style=default_config["panel_border"])
+    table.add_column("IP", style="cyan", no_wrap=True)
+    table.add_column("MAC", style="magenta", no_wrap=True)
+
+    for device in discover_devices():
+        table.add_row(device["IP"], device["MAC"])
+    
+    return table
+
+# Painel para gráficos de pacotes
+def draw_graph_panel(data, graph_name, color):
+    acp_config = {
+        "width": default_config["graph_width"],  # Aumentando o tamanho do gráfico
+        "format": "{:5.0f} p/s",  # Converte o float para inteiro sem erro
+    }
+
+    return Panel(
+        Align.left(acp.plot(data, acp_config), vertical="bottom"),
+        title=f"[bold][yellow]{graph_name}[/bold][/yellow]",
+        border_style="#d600ff",
+        style=color,
+    )
+
+# Sair do programa
+def exit_app():
+    global exit_script
+    exit_script = True
+    os._exit(0)
+
+# Controle do usuário
+def user_controls():
+    kb.add_hotkey("tab", exit_app)
+
+# Função para exibir as últimas informações de pacotes
+def create_packet_info_panel():
+    panel_content = "\n".join(
+        [f"[bold][yellow]Origem:[/bold][/yellow] {packet['src']} - [bold][yellow]Destino:[/bold][/yellow] {packet['dst']}" 
+         for packet in last_10_packets]
+    )
+    
+    return Panel(
+        Align.center(panel_content),
+        border_style=default_config["panel_border"],
+        title="[bold][yellow]Últimos 10 Pacotes[/bold][/yellow]",
+    )
+
+# Loop principal da interface
+def main():
+    global exit_script, packet_buffer_in, packet_buffer_out, last_10_packets
+
+    layout = Layout()
+    layout.split(
+        Layout(name="header", size=3),  # O header agora ocupa toda a largura disponível
+        Layout(name="main", ratio=1),
+        Layout(name="footer", size=3),
+    )
+    layout["main"].split_row(Layout(name="info", ratio=2), Layout(name="graphs", ratio=3))  # Aumenta a proporção dos gráficos
+    layout["graphs"].split_column(Layout(name="recv_graph"), Layout(name="sent_graph"))
+    layout["info"].split(Layout(name="net_usage", size=3), Layout(name="devices_table", ratio=1), Layout(name="packet_info", ratio=1))
+
+    layout["header"].update(
+        Panel(
+            Align.center("[bold][yellow] Monitoramento de Rede [/yellow][/bold]"),
+            border_style=default_config["panel_border"],
+        )
+    )
+    layout["footer"].update(
+        Panel(
+            Align.left("[bold][yellow]Tab para Sair[/yellow][/bold]"),
+            title="Controles",
+            border_style=default_config["panel_border"],
+        )
+    )
+
+    user_controls()
+
+    # Inicia sniffing em uma thread separada
+    sniff_thread = threading.Thread(target=start_sniffing, daemon=True)
+    sniff_thread.start()
+
+    with Live(layout, refresh_per_second=60, screen=True) as live:
+        while not exit_script:
+            # Atualiza os buffers de pacotes
+            packet_buffer_in.append(packet_counts["incoming"])
+            packet_buffer_out.append(packet_counts["outgoing"])
+
+            # Limpa os contadores de pacotes para a próxima captura
+            packet_counts["incoming"] = 0
+            packet_counts["outgoing"] = 0
+
+            # Atualiza o layout com os gráficos, informações de rede, dispositivos conectados e últimos pacotes
+            layout["recv_graph"].update(
+                draw_graph_panel(packet_buffer_in, "Pacotes Recebidos", "#00ff9f")
+            )
+            layout["sent_graph"].update(
+                draw_graph_panel(packet_buffer_out, "Pacotes Enviados", "#00b8ff")
+            )
+            layout["net_usage"].update(
+                Panel(
+                    Align.center(
+                        f"IN: [bold][green]{packet_buffer_in[-1]}[/green][/bold] p/s OUT: [bold][red]{packet_buffer_out[-1]}[/red][/bold] p/s"
+                    ),
+                    border_style=default_config["panel_border"],
+                    title="[bold][yellow]Tráfego de Rede[/bold][/yellow]",
+                )
+            )
+            layout["devices_table"].update(
+                Panel(
+                    Align.center(create_device_table()),
+                    border_style=default_config["panel_border"],
+                    title="[bold][yellow]Dispositivos Conectados[/bold][/yellow]",
+                )
+            )
+            layout["packet_info"].update(create_packet_info_panel())
+
+if __name__ == "__main__":
+    # Use essa variável para montorar 2 hosts. Caso seja vazio, monitora a rede inteira.
+    # MONITORED_HOSTS = ["52.67.164.66", "192.168.0.2"]  # Ajuste conforme necessário
+    main()
